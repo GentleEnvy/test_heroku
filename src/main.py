ca503d948ff3e2a40b3db871715481d4d214fce6
Envy <envy15@mail.ru>
2020-12-31 11:54:44 +0300

    emulate egeshka 4

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from src.app import app, run_configuration
 from src.urls import init_urls
 
-print(__name__)
-if __name__ == '__main__':
-    init_urls(app)
-    app.run(**run_configuration)
+init_urls(app)
+app.run(**run_configuration)
