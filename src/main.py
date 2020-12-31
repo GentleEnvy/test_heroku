ade588dee12a74071b2f55f13f973b06ab43a993
Envy <envy15@mail.ru>
2020-12-31 13:36:06 +0300

    test flask 3

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 import flask
 
+
+class MyApp(flask.Flask):
+
+    def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
+        print(f'run: {self.name = }')
+        print(f'run: __name__: {__name__}')
+        super().run(host, port, debug, load_dotenv, **options)
+
+
 print(f'__name__ = {__name__}')
-app = flask.Flask(__name__)
+app = MyApp(__name__)
 print(f'create app: {app.name}')
 
 
@@ -12,4 +21,4 @@ def index():
 
 if __name__ == '__main__':
     print(f'running: {__name__}')
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5002)
