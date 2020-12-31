23d77f4b116bc2cc8e6bf0200a77c4d8f41d522d
Envy <envy15@mail.ru>
2020-12-31 13:23:54 +0300

    #

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
-from flask import Flask
+import flask
 
-from src.app import run_configuration
-from src.urls import init_urls
-
-# init_urls(app)
-
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, world !'
+    return 'Hello, World !'
 
 
-app.run()
+print(__name__)
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
