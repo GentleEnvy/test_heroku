60522542f98691e707c96ba0f4d10d01e13c66b6
Envy <envy15@mail.ru>
2020-12-31 13:19:49 +0300

    emulate egeshka 7

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,5 +2,5 @@ from ._app import create_app, read_run_configuration
 
 __all__ = ['app', 'run_configuration']
 
-app = create_app()
+# app = create_app()
 run_configuration = read_run_configuration()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-from src.app import app, run_configuration
+from flask import Flask
+
+from src.app import run_configuration
 from src.urls import init_urls
 
-init_urls(app)
+# init_urls(app)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, world !'
+
+
 app.run()
