d9e658ac6284d1a107f8f32ca4ef080698a92dbe
Envy <envy15@mail.ru>
2020-12-31 11:01:45 +0300

    getcwd

--- test_heroku/app/app.py
+++ test_heroku/app/app.py
@@ -5,5 +5,6 @@ import os
 
 def create_app():
     app = Flask('__main__')
-    app.config.from_json('config.json')
+    print(f'{os.getcwd()}/app/config.json')
+    app.config.from_json(f'{os.getcwd()}/app/config.json')
     return app
