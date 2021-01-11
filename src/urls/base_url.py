8b8c4e7c39c90eb398c747b1d6eaee1b835aaaaa
Envy <envy15@mail.ru>
2021-01-11 18:56:22 +0300

    test server 5

--- src/urls/base_url.py
+++ src/urls/base_url.py
@@ -33,7 +33,6 @@ class BaseUrl(ABC, metaclass=_MetaBaseUrl):
         def index() -> Response:
             reply = self.reply(flask_request)
             resp = app.make_response(reply)
-            resp.status_code = 404
             return resp
 
     @property
