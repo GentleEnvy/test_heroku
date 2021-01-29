ff7c8c2468f9f05694dc53a9772b73aadd37703e
Envy <envy15@mail.ru>
2021-01-29 22:20:17 +0300

    test logging 3

--- src/utils/logs/print_logs.py
+++ src/utils/logs/print_logs.py
@@ -6,7 +6,7 @@ from src.utils import get_path_to_src
 
 def _print_logs():
     while True:
-        path_to_logs = get_path_to_src() + '/logs.log'
+        path_to_logs = get_path_to_src() + '/utils/logs/logs.log'
         try:
             with open(path_to_logs, 'r') as logs:
                 print('logs: <<<', logs.read(), '>>>')
