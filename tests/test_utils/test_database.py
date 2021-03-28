2df74e6b42edd25fd125e912b73c5292c9709acf
Envy <envy15@mail.ru>
2021-03-28 18:52:02 +0300

    testing 2

--- tests/test_utils/test_database.py
+++ tests/test_utils/test_database.py
@@ -3,7 +3,3 @@ from src.utils import database
 
 def test_select():
     assert database.execute(f'SELECT id FROM "user"') == ((1,),)
-
-
-def test_error():
-    database.execute('SELECT * FROM non_existent_table')
